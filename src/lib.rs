b5669a8a1372a27c21c73254c98a7f6202376f52
Chris de la Iglesia <christopher.delaiglesia@gmail.com>
2019-05-13 13:39:26 -0700

    Support basic fights

--- src/lib.rs
+++ src/lib.rs
@@ -1,84 +1,119 @@
 // Chris de la Iglesia, 2019
 
-use std::collections::HashMap;
+extern crate serde;
+extern crate rand;
+
+use rand::prelude::*;
+use rand::{thread_rng, Rng};
 use std::vec::Vec;
 use std::result::Result;
 use serde::Deserialize;
 
+#[derive(Deserialize, Debug)]
 pub struct Attack {
+    #[serde(default)]
     name: String,
+    #[serde(default)]
     to_hit: i64,
-//    damage: String,
+    #[serde(default)]
     base_damage: i64,
+    #[serde(default)]
     num_d4: i64,
+    #[serde(default)]
     num_d6: i64,
+    #[serde(default)]
     num_d8: i64,
+    #[serde(default)]
     num_d10: i64,
+    #[serde(default)]
     num_d12: i64,
+    #[serde(default)]
     num_d20: i64,
 }
 
-#[derive(serde::Deserialize)]
+#[derive(Deserialize, Debug)]
 pub struct StatBlock {
+    #[serde(default)]
+    name: String,
+    #[serde(default)]
     max_hp: i64,
-    to_hit: i64,
+    #[serde(default)]
     armor: i64,
+    #[serde(default)]
     attacks: Vec<Attack>,
 }
 
-pub fn tokenize(input: &str) -> Result<Vec<String>,&str> {
-    let mut text = input;
-    text = text.trim();
-    let mut output = Vec::new();
-    while text.len() > 0 {
-        let next = &(text[0..1]);
-        if "{}\",:".contains(next) {
-            output.push(String::from(next));
-            text = &text[1..];
-            continue;
+impl StatBlock {
+    pub fn from_str(string: &str) -> Result<StatBlock,serde_json::error::Error> {
+        return serde_json::from_str(string);
+    }
+}
+
+fn roll_dice(size: i64, num: i64, rng: &mut ThreadRng) -> i64 {
+    let mut total = 0;
+    for i in (0..num) {
+        total += rng.gen_range(1,size);
+    }
+    total
+}
+
+fn hits(attack: &Attack, creature: &StatBlock, rng: &mut ThreadRng) -> bool {
+    rng.gen_range(1,21) + attack.to_hit >= creature.armor
+}
+
+fn damage(attack: &Attack, rng: &mut ThreadRng) -> i64 {
+    // TODO: add in dice rolls
+    let mut dmg = attack.base_damage;
+    dmg += roll_dice(4, attack.num_d4, rng);
+    dmg += roll_dice(6, attack.num_d6, rng);
+    dmg += roll_dice(8, attack.num_d8, rng);
+    dmg += roll_dice(10, attack.num_d10, rng);
+    dmg += roll_dice(12, attack.num_d12, rng);
+    dmg += roll_dice(20, attack.num_d20, rng);
+    dmg
+}
+
+pub fn fight(creature1: &StatBlock, creature2: &StatBlock) -> Option<String> {
+    let mut rng = rand::thread_rng();
+    let attack1 = match creature1.attacks.first() {
+        Some(x) => {
+            x
         }
-        if let Some(target_i) = text.find(|c: char| "{}\",:".contains(c)) {
-            output.push(String::from(&(text[0..target_i])));
-            text = &text[target_i..];
+        None => {
+            return None;
+        }
+    };
+    let attack2 = match creature2.attacks.first() {
+        Some(x) => x,
+        None => {return None;}
+    };
+    let mut creature1dmg = 0;
+    let mut creature2dmg = 0;
+    loop {
+        if creature1dmg >= creature1.max_hp {
+            println!("Creature 2 won!");
+            break;
+        }
+        if hits(attack1, creature2, &mut rng) {
+            let dmg = damage(attack1, &mut rng);
+            println!("{} (1) does {} damage from {}", creature1.name, dmg, attack1.name);
+            creature2dmg += dmg;
         } else {
-            return Err("Error tokenizing");
+            println!("{} (1) misses with {}", creature1.name, attack1.name);
+        }
+        
+        if creature2dmg >= creature2.max_hp {
+            println!("Creature 1 won!");
+            break;
+        }
+        if hits(attack2, creature2, &mut rng) {
+            let dmg = damage(attack1, &mut rng);
+            println!("{} (2) does {} damage from {}", creature2.name, dmg, attack2.name);
+            creature1dmg += dmg;
+        } else {
+            println!("{} (2) misses with {}", creature2.name, attack2.name);
         }
     }
-    return Ok(output);
-}
-
-pub fn parse_json(input: &str) -> Result<HashMap<String,String>,String> {
-    let tokens = tokenize(input)?;
-    let mut i = 0;
-    parse_object(&tokens, &mut i)?;
-    return Ok(HashMap::new());
-}
-
-fn expect(input: &Vec<String>, i: &mut usize, value: &str) -> Result<String,String> {
-    if *i > input.len() {
-        return Err("Not enough tokens".to_string());
-    }
-    let target = &input[*i];
-    if value == target {
-        *i += 1;
-        Ok(target.to_string())
-    } else {
-        Err("Expected value not there".to_string())
-    }
-}
-
-fn parse_object(input: &Vec<String>, i: &mut usize) -> Result<HashMap<String,String>,String> {
-    expect(input, i, "{")?;
-    let key = parse_key(input, i)?;
-    expect(input, i, "}")?;
-    Ok(HashMap::new())
-}
-
-fn parse_key<'a>(input: &'a Vec<String>, i: &mut usize) -> Result<String,String> {
-    expect(input, i, "\"");
-    let key = input[*i].to_string();
-    *i += 1;
-    expect(input, i, "\"");
-    Ok(key)
+    Some(String::from("Done"))
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 // Chris de la Iglesia
 
 extern crate rand;
-//extern crate serde;
-//extern crate serde_json;
+extern crate serde;
+extern crate serde_json;
 
 use serde::Serialize;
 
@@ -15,13 +15,16 @@ fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
     println!("Args: {:?}", args);
     //let search_string = &args[1];
-    let mut file = File::open(&args[1])?;
+    let file = File::open(&args[1])?;
     let mut file = BufReader::new(file);
     let mut content = String::new();
     file.read_to_string(&mut content)?;
     println!("Content: {}", content);
-    let tokens = monsterattack::tokenize(&content);
-    println!("Tokens: {:?}", tokens);
-    monsterattack::parse_json(&content).expect("Failed to parse");
+    //let tokens = monsterattack::tokenize(&content);
+    //println!("Tokens: {:?}", tokens);
+    //monsterattack::parse_json(&content).expect("Failed to parse");
+    let human = monsterattack::StatBlock::from_str(&content)?;
+    println!("Parsed: {:?}", monsterattack::StatBlock::from_str(&content)?);
+    monsterattack::fight(&human, &human);
     Ok(())
 }
